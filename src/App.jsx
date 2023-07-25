bccd1749f500d592dd183955e929c3d893279c17
KshitijV97 <writetokshitij@gmail.com>
2023-07-25 18:48:14 +0530

    Code splitting on basis of route

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
+import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import NavWrapper from "./components/NavWrapper/NavWrapper";
 import "./App.css";
-import Home from "./components/Home/Home";
-import Store from "./components/Store/Store";
-import About from "./components/About/About";
+
+const Home = React.lazy(() => import("./components/Home/Home"));
+const Store = React.lazy(() => import("./components/Store/Store"));
+const About = React.lazy(() =>
+	import("./components/About/About").then((module) => {
+		return { default: module.About };
+	})
+);
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<NavWrapper />}>
-					<Route path='/' element={<Home />} />
-					<Route path='/store' element={<Store />} />
-					<Route path='/about' element={<About />} />
-				</Route>
-			</Routes>
+			<React.Suspense fallback={<h1>Loading....</h1>}>
+				<Routes>
+					<Route path='/' element={<NavWrapper />}>
+						<Route path='/' element={<Home />} />
+						<Route path='/store' element={<Store />} />
+						<Route path='/about' element={<About />} />
+					</Route>
+				</Routes>
+			</React.Suspense>
 		</BrowserRouter>
 	);
 }
